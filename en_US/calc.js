a5e801989875cc0d0b161f09b44518ee6a08b248
Slushie <suhailm0608@gmail.com>
2022-09-11 23:14:04 +0100

    test

--- en_US/calc.js
+++ en_US/calc.js
@@ -14,7 +14,7 @@ angular
     $scope.loadout = new Loadout();
     $scope.status = {}
     $scope.logging = false;
-    $scope.appVersion = 230; // 2.X.X
+    $scope.appVersion = 100; // 2.X.X
     $scope.screenshotMode = false;
     $scope.toggledAbilities = {};
 
